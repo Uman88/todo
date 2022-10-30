326894aa43a8c944456dafd4a78f7d0d487c9bac
Victor Umanskiy <10357172+Uman88@users.noreply.github.com>
2022-10-30 23:39:16 +0200

    In the file login change icon on email and in the input type to password

--- app/page/login.php
+++ app/page/login.php
@@ -14,12 +14,12 @@
         <form action="/index.php?route=signin" method="post" enctype="multipart/form-data" class="sign-in-form">
             <h2 class="title-form">Авторизация</h2>
             <div class="input-field">
-                <i class="ri-user-line"></i>
-                <input type="text" name="name" placeholder="Введите имя">
+                <i class="ri-mail-line"></i>
+                <input type="email" name="email" placeholder="Введите email">
             </div>
             <div class="input-field">
                 <i class="ri-lock-line"></i>
-                <input type="text" name="password" placeholder="Введите пароль">
+                <input type="password" name="password" placeholder="Введите пароль">
             </div>
             <input type="submit" value="Авторизоваться" class="btn-reg-login">
             <p>У вас нет аккаунта? <a href="/index.php?route=register" class="account-text">Зарегистрируйтесь</a></p>
