274838f9d9bc8b56dfb322bed6324a50e971f195
Victor Umanskiy <10357172+Uman88@users.noreply.github.com>
2022-11-01 01:50:17 +0200

    Aded two constants

--- app/core/config.php
+++ app/core/config.php
@@ -3,3 +3,5 @@
 define('ROOT', $_SERVER['DOCUMENT_ROOT'] .'/');
 define('CORE', $_SERVER['DOCUMENT_ROOT'] .'/app/core/');
 define('APP_NAME', "TODO");
+define('UPLOADS','/public/assets/uploads/');
+define('IMAGES','/public/assets/images/');
