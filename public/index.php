0b6e9f90b359643d6f046c27f6a79dba471ec832
Victor Umanskiy <10357172+Uman88@users.noreply.github.com>
2022-11-10 00:25:56 +0200

    Correct loading of the main page

--- public/index.php
+++ public/index.php
@@ -6,11 +6,16 @@ require_once '../app/core/init.php';
 
 $route = isset($_GET['route']) ? $_GET['route'] : 'todo';
 
-if (file_exists('../app/page/' . $route . '.php')) {
-    require_once '../app/page/' . $route . '.php';
-} elseif (file_exists('../app/core/' . $route . '.php')) {
-    require_once '../app/core/' . $route . '.php';
+if ($_SERVER['REQUEST_URI'] === '/') {
+    // По умолчанию загружается категория id=2, при загрузке index страницы.
+    header('Location: index.php?id=2');
 } else {
-    http_response_code(404);
-    require_once '../app/page/404.php';
+    if (file_exists('../app/page/' . $route . '.php')) {
+        require_once '../app/page/' . $route . '.php';
+    } elseif (file_exists('../app/core/' . $route . '.php')) {
+        require_once '../app/core/' . $route . '.php';
+    } else {
+        http_response_code(404);
+        require_once '../app/page/404.php';
+    }
 }
