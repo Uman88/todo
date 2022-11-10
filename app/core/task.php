52d7cb87bf4d1a248b57ce15974f2a05b550eef6
Victor Umanskiy <10357172+Uman88@users.noreply.github.com>
2022-11-10 02:34:01 +0200

    Correct save task in the db

--- app/core/task.php
+++ app/core/task.php
@@ -3,12 +3,15 @@
 require_once 'connection.php';
 
 $task = filter_var(trim($_POST['task']), FILTER_SANITIZE_STRING);
+$category = filter_var($_POST['select-category'], FILTER_SANITIZE_NUMBER_INT);
+$priority = filter_var($_POST['select-priority'], FILTER_SANITIZE_NUMBER_INT);
+$user_id = $_SESSION['user']['id'];
 $date = date('m-d-Y');
 
-$id = '1';
+mysqli_query($connect, "
+INSERT INTO 
+    `tasks` (`title`, `id_category`, `id_user`, `priority`) 
+VALUES 
+    ('$task', '$category', '$user_id', '$priority')");
 
-$user_id = $_SESSION['user']['id'];
-
-$result = mysqli_query($connect, "INSERT INTO `tasks` (`title`, `id_category`, `id_user`) VALUES ('$task', '$id', '$user_id')");
-
-header('Location: /index.php?route=todo');
+header('Location: /');
