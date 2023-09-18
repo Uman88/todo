b543600e4d3fc1ac368d1ffbacce4dd70e296e95
Victor Umanskiy <10357172+Uman88@users.noreply.github.com>
2023-09-18 13:23:05 +0300

    renamed handler in the todo

--- app/page/todo.php
+++ app/page/todo.php
@@ -5,19 +5,22 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TODO List</title>
-    <?php include 'includes/css.php' ?>
+    <title><?= APP_NAME; ?></title>
+    <?php include INCLUDES . '/css.php' ?>
 </head>
 <body>
 
 <?php
+
 if (isset($_SESSION['user'])) {
-    include 'includes/header.php';
-    include 'includes/sidebar.php';
-    include 'includes/content.php';
+    include INCLUDES . '/header.php';
+    include INCLUDES . '/sidebar.php';
+    include INCLUDES . '/content.php';
+} else {
+    include 'login.php';
 }
 ?>
 
-<?php include 'js.php'; ?>
+<?php include INCLUDES . '/js.php'; ?>
 </body>
 </html>
