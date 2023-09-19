244891b2af083a939746691d3f9cce923a37cd55
Victor Umanskiy <10357172+Uman88@users.noreply.github.com>
2023-09-19 20:53:48 +0300

    counted the number of categories for each categorys

--- app/page/includes/sidebar.php
+++ app/page/includes/sidebar.php
@@ -2,13 +2,27 @@
 <aside class="sidebar" id="sidebar">
     <ul class="sidebar-list">
         <?php
-        $cats = mysqli_query($conn, "SELECT * FROM `category`");
-        if (mysqli_num_rows($cats) > 0) :
-            while ($cat = mysqli_fetch_assoc($cats)) : ?>
+        $sql = "SELECT * FROM `category`";
+        $cats = $conn->query($sql);
+        if ($cats->num_rows > 0) :
+            while ($cat = $cats->fetch_assoc()) : ?>
                 <li class="sidebar-list-item">
                     <?= $cat['title'] == 'Сегодня' ? '<span class="calendar-date">' . date("j") . '</span>' : ''; ?>
                     <span class="material-symbols-outlined"><?= $cat['tag']; ?></span>
-                    <a href="index.php?id=<?= $cat['id']; ?>"><?= $cat['title']; ?></a>
+                    <a href="index.php?id=<?= $cat['id']; ?>"><?= $cat['title']; ?>
+                        <span class="qty-tasks">
+                        <?php
+                        $id_cat = $cat['id'];
+                        $id_user = $_SESSION['user']['id'];
+                        $sql2 = "SELECT count(*) FROM `task` WHERE `category`='$id_cat' AND `id_user`='$id_user'";
+                        $count = $conn->query($sql2);
+                        $num = $count->fetch_row();
+                        if (!empty($num[0])) {
+                            echo $num[0];
+                        }
+                        ?>
+                        </span>
+                    </a>
                 </li>
             <?php
             endwhile; endif; ?>
