5d024d5f60ef3f5f504ddb406cdd7a43ed97747b
Victor Umanskiy <10357172+Uman88@users.noreply.github.com>
2023-09-27 23:41:15 +0300

    view profile

--- app/page/profile.php
+++ app/page/profile.php
@@ -1,1 +1,32 @@
-Привет юзер
+<?php
+
+require_once '../app/core/init.php';
+
+if (empty($_SESSION['user'])) {
+    header('Location: index.php?route=login');
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include INCLUDES . '/css.php'; ?>
+    <title>Профиль</title>
+</head>
+<body>
+<?php
+if (isset($_SESSION['user'])) {
+    include INCLUDES . '/header.php';
+    include INCLUDES . '/sidebar.php';
+    include INCLUDES . '/profile.php';
+} else {
+    include 'login.php';
+}
+?>
+
+<?php include INCLUDES . '/js.php'; ?>
+</body>
+</html>
